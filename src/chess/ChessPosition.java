22f7ddeea83d352dee751b5f9e46e966340c42cf
Aldrin Ricardo <aldrinricardo96@gmail.com>
2022-09-08 13:21:48 -0300

    ChessException and ChessPosition

---
+++ src/chess/ChessPosition.java
@@ -0,0 +1,40 @@
+package chess;
+
+import boardgame.Position;
+
+public class ChessPosition {
+
+	private char column;
+	private int row;
+	
+	public ChessPosition(char column, int row) {
+		if (column < 'a' || column > 'h' || row < 1 || row > 8) {
+			throw new ChessException("Error! Please insert a value from a1 to h8");
+		}
+		this.column = column;
+		this.row = row;
+	}
+
+	public char getColumn() {
+		return column;
+	}
+
+	public int getRow() {
+		return row;
+	}
+	
+	//Converts a ChessPosition and returns as a Position
+	protected Position toPosition() {
+		return new Position(8 - row, column - 'a');
+	}
+	
+	//Converts a Position and return as a ChessPosition
+	protected static ChessPosition fromPosition(Position position) {
+		return new ChessPosition((char) ('a' - position.getColumn()), 8 - position.getRow());
+	}
+	
+	@Override
+	public String toString() {
+		return "" + column + row;
+	}
+}
